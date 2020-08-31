490f74ac16d37648d8317c11e7d3822be9aac9da
pbaldassari <68169796+pbaldassari@users.noreply.github.com>
2020-08-30 20:00:22 -0700

    Code update of forecast

--- index.js
+++ index.js
@@ -1,77 +1,59 @@
-//Feature 1
-let currentDate = new Date();
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
 
-let weekDay = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
-let months = [
-  "Jan",
-  "Feb",
-  "Mar",
-  "Apr",
-  "May",
-  "Jun",
-  "Jul",
-  "Aug",
-  "Sep",
-  "Oct",
-  "Nov",
-  "Dec",
-];
-
-let newTime = document.querySelector("#time");
-let currentTime = currentDate.toLocaleTimeString([], {
-  hour12: false,
-  hour: "2-digit",
-  minute: "2-digit",
-});
-let date = currentDate.getDate();
-let month = months[currentDate.getMonth()];
-let currentDay = weekDay[currentDate.getDay()];
-
-newTime.innerHTML = `${currentDay}, ${month} ${date}  | ${currentTime}`;
-
-//Search Bar
-function citySearch(event) {
-  event.preventDefault();
-  let cityName = document.querySelector("#name-city");
-  let cityInput = document.querySelector("#city-input");
-
-  cityName.innerHTML = cityInput.value.toUpperCase();
+  let weekDay = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+  let month = months[date.getMonth()];
+  let days = weekDay[date.getDay()];
+  let numberDay = date.getDate();
+  let currentTime = date.toLocaleTimeString([], {
+    hour12: true,
+    hour: "2-digit",
+    minute: "2-digit",
+  });
+  return `${days}, ${month} ${numberDay}  |  ${currentTime}`;
 }
 
-let searchForm = document.querySelector("#form-input");
-searchForm.addEventListener("submit", citySearch);
+function formatHours(timestamp) {
+  let date = new Date(timestamp);
 
-//Farenheit and Celsius
-function cTemp() {
-  let currentCTemp = document.querySelector(".degrees");
-  currentCTemp.innerHTML = "10°C";
+  let currentTime = date.toLocaleTimeString([], {
+    hour12: true,
+    hour: "2-digit",
+    minute: "2-digit",
+  });
+  return `${currentTime}`;
 }
-let showCTemp = document.querySelector("#celsius-link");
-showCTemp.addEventListener("click", cTemp);
 
-function fTemp() {
-  let currentFTemp = document.querySelector(".degrees");
-  currentFTemp.innerHTML = "50°F";
-}
-let showFTemp = document.querySelector("#fahrenheit-link");
-showFTemp.addEventListener("click", fTemp);
-
-/// Week 5 Homework
 function showWeather(response) {
-  console.log(response);
   let temperature = Math.round(response.data.main.temp);
   let windSpeed = Math.round(response.data.wind.speed);
   let humidity = Math.round(response.data.main.humidity);
   let currentCity = response.data.name;
   let description = response.data.weather[0].description;
+
+  fahrenheitTemp = response.data.main.temp;
 
   let h3 = document.querySelector("h3");
   h3.innerHTML = `${temperature}°F`;
@@ -85,43 +67,88 @@ function showWeather(response) {
   let currentLocation = document.querySelector("h1");
   currentLocation.innerHTML = `${currentCity.toUpperCase()}`;
 
-  let weatherType = document.querySelector("#weather");
+  let weatherType = document.querySelector("#description");
   weatherType.innerHTML = `${description.toUpperCase()}`;
+
+  let iconElement = document.querySelector("#icon");
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+
+  let dateElement = document.querySelector("#time");
+  //dateElement.innerHTML = formatDate;
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
-//let currentLocationButton = document.querySelector("#current-button");
-//currentLocationButton.addEventListener("click", showWeather);
+function displayForecast(response) {
+  let forecastPanel = document.querySelector("#weather-forecast");
+  forecastPanel.innerHTML = null;
+  let forecast = null;
 
-function searchCity(city) {
+  for (let index = 0; index < 6; index++) {
+    forecast = response.data.list[index];
+
+    forecastPanel.innerHTML += `
+    <div class="col-lg-2">
+                <h2>${formatHours(forecast.dt * 1000)}</h2>
+                <img
+                  src= "http://openweathermap.org/img/wn/${
+                    forecast.weather[0].icon
+                  }@2x.png"
+                  
+                  />
+                
+                <p class="card-text">${Math.round(
+                  forecast.main.temp_max
+                )}°<span class="night">/ ${Math.round(
+      forecast.main.temp_min
+    )}°</span></p>
+              </div>`;
+  }
+}
+
+function search(city) {
   let apiKey = "74b46494188e9abc362ff59069b258f0";
   let units = "imperial";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=${units}&appid=${apiKey}`;
-
   axios.get(apiUrl).then(showWeather);
+
+  apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=${units}&appid=${apiKey}`;
+  axios.get(apiUrl).then(displayForecast);
 }
 
-function search(event) {
+function handleSubmit(event) {
   event.preventDefault();
-  let searchCityName = document.querySelector("#city-input").value;
-  searchCity(searchCityName);
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
 }
 
-function displayPosition(position) {
-  let latitude = position.coords.latitude;
-  let longitude = position.coords.longitude;
-  let units = "imperial";
-  let apiKey = "74b46494188e9abc362ff59069b258f0";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=${units}`;
-
-  axios.get(apiUrl).then(showWeather);
-}
-let currentLocationsButton = document.querySelector("#current-button");
-currentLocationsButton.addEventListener("click", getCurrentLocation);
-
-function getCurrentLocation(event) {
+function displayCelsiusTemp(event) {
   event.preventDefault();
-  navigator.geolocation.getCurrentPosition(displayPosition);
+  let degreesElement = document.querySelector("h3");
+  fahrenheitLink.classList.remove("active");
+  celsiusLink.classList.add("active");
+  let celsiusTemp = ((fahrenheitTemp - 32) * 5) / 9;
+  degreesElement.innerHTML = `${Math.round(celsiusTemp)}°C`;
 }
+
+function displayFahrenheitTemp(event) {
+  event.preventDefault();
+  let degreesElement = document.querySelector("h3");
+  fahrenheitLink.classList.add("active");
+  celsiusLink.classList.remove("active");
+
+  degreesElement.innerHTML = `${Math.round(fahrenheitTemp)}°F`;
+}
+let fahrenheitTemp = null;
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", displayFahrenheitTemp);
 
 let form = document.querySelector("#form-input");
-form.addEventListener("submit", search);
+form.addEventListener("submit", handleSubmit);
+
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", displayCelsiusTemp);
+
+search("Los Angeles");
