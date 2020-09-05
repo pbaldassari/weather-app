2831e3c722fb60b3aa9861f72f73b5a88a9d3b9c
pbaldassari <68169796+pbaldassari@users.noreply.github.com>
2020-09-05 14:10:31 -0700

    Updated current location code

--- index.js
+++ index.js
@@ -1,21 +1,3 @@
-//Current Locaiton
-function displayPosition(position) {
-  let latitude = position.coords.latitude;
-  let longitude = position.coords.longitude;
-  let units = "imperial";
-  let apiKey = "74b46494188e9abc362ff59069b258f0";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=${units}`;
-
-  axios.get(apiUrl).then(showWeather);
-}
-let currentLocationsButton = document.querySelector("#current-button");
-currentLocationsButton.addEventListener("click", getCurrentLocation);
-
-function getCurrentLocation(event) {
-  event.preventDefault();
-  navigator.geolocation.getCurrentPosition(displayPosition);
-}
-
 function formatDate(timestamp) {
   let date = new Date(timestamp);
 
@@ -167,5 +149,25 @@ form.addEventListener("submit", handleSubmit);
 
 let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemp);
+
+//Current Locaiton
+function displayPosition(position) {
+  let latitude = position.coords.latitude;
+  let longitude = position.coords.longitude;
+  let units = "imperial";
+  let apiKey = "74b46494188e9abc362ff59069b258f0";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=${units}`;
+
+  axios.get(apiUrl).then(showWeather);
+  let newApiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=${units}`;
+  axios.get(newApiUrl).then(displayForecast);
+}
+let currentLocationsButton = document.querySelector("#current-button");
+currentLocationsButton.addEventListener("click", getCurrentLocation);
+
+function getCurrentLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(displayPosition);
+}
 
 search("Los Angeles");
